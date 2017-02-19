078b5657730c45144e9b0bea01896ec2b416ea6f
Brian <brianleediablo3@hotmail.com>
2017-02-18 18:21:49 -0800

    Added new pages , fonts and major   changes.

--- themes/inhab/gulpfile.js
+++ themes/inhab/gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('browser-sync', function () {
     ];
 
     browserSync.init(files, {
-        proxy: 'localhost.80/inhab',
+        proxy: 'localhost/inhab',
     });
 
     gulp.watch(files).on('change', browserSync.reload);
